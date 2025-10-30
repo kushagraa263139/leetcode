677e6182ed820083a864f535de7e4e789b1cc181
kushagraa263139 <kushagra263139@gmail.com>
2025-10-30 21:22:16 +0530

    Time: 7 ms (99.70%) | Memory: 58.1 MB (11.45%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,33 +1,25 @@
 class Solution {
-    public Boolean isSubsetSum(int arr[], int sum) {
-        
-        boolean [][] dp = new boolean[arr.length+1][sum+1];
-        int n = dp.length;
-        // set first col as true becoz sum 0 can be generated easily and always
-        
-        for(int row=0;row<dp.length;row++){
-            dp[row][0]=true;
-        }
-        
-        for(int row=n-2;row>=0;row--){
-            for(int col=1;col<sum+1;col++){
-                
-                boolean res1=false,res2=false;
-                // take it
-                if(col>=arr[row]) res1=dp[row+1][col-arr[row]];
-                // skip it
-                res2=dp[row+1][col];
-                
-                dp[row][col]=(res1||res2);
-            }
-        }
-        
-        return dp[0][sum];
-    }
     public boolean canPartition(int[] nums) {
         int sum=0;
-        for(int i:nums) sum=sum+i;
-        if(sum%2!=0) return false;
-        return isSubsetSum(nums,sum/2);
+        for(int num:nums){
+            sum+=num;
+        }
+        if(sum%2 != 0)
+            return false;
+        int n=nums.length;
+        Boolean[][] dp = new Boolean[n][sum/2+1];
+        return sum(nums,n,1,sum/2,dp);
+
+        // return dp[nums.length-1][sum/2];
+    }
+
+    private boolean sum(int[] nums, int n, int i, int sum, Boolean[][] dp){
+        if(sum==0)
+            return true;
+        if(i==n || sum<0)
+            return false;
+        if(dp[i][sum]!=null)
+            return dp[i][sum];
+        return dp[i][sum] = sum(nums, n, i+1, sum-nums[i], dp) || sum(nums, n, i+1, sum, dp);
     }
 }
