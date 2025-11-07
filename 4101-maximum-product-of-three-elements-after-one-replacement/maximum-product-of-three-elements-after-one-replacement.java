b52655279906af7e636bf864cf7f34e142a1942b
kushagraa263139 <kushagra263139@gmail.com>
2025-11-07 23:43:43 +0530

    Time: 2 ms (100.00%) | Memory: 97.2 MB (36.33%) - LeetSync

--- 4101-maximum-product-of-three-elements-after-one-replacement/maximum-product-of-three-elements-after-one-replacement.java
+++ 4101-maximum-product-of-three-elements-after-one-replacement/maximum-product-of-three-elements-after-one-replacement.java
@@ -1,12 +1,25 @@
-import java.util.Arrays;
-
+import java.lang.*;
 class Solution {
     public long maxProduct(int[] nums) {
+
+        long firstMax=Integer.MIN_VALUE;
+        long secondMax=Integer.MIN_VALUE;
+        long firstMin=Integer.MAX_VALUE;
+        long secondMin=Integer.MAX_VALUE;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]<0) nums[i]=nums[i]*-1;
+            if(nums[i]>=firstMax){
+                secondMax=firstMax;
+                firstMax=nums[i];
+            }else if (nums[i]>secondMax){
+                secondMax = nums[i];
+            }
+            if(nums[i]<=firstMin){
+                secondMin=firstMin;
+                firstMin=nums[i];
+            }else if (nums[i]<secondMin){
+                secondMin = nums[i];
+            }
         }
-        Arrays.sort(nums);
-        return 100000L*nums[nums.length-1]*nums[nums.length-2];
-        
+        return 100000*Long.max(Math.abs(firstMax*secondMax), Long.max(Math.abs(firstMax*firstMin), Math.abs(firstMin*secondMin)));
     }
 }
